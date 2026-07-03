bb34841681c26b7c830f0f9d2ca5ab91273a9d86
Carlosss2 <233286@ids.upchiapas.edu.mx>
2026-07-03 12:54:23 -0600

    fix in transcriptor

--- lib/features/transcription/data/datasources/feedback_remote_datasource.dart
+++ lib/features/transcription/data/datasources/feedback_remote_datasource.dart
@@ -5,7 +5,7 @@ import '../../domain/entities/study_plan_entity.dart';
 
 class FeedbackRemoteDataSource {
   static const _baseUrl =
-      'https://b5w3o5l2bxdzw7-8080.proxy.runpod.net';
+      'https://9awbvpdnfjjs3v-8080.proxy.runpod.net';
 
   Future<FeedbackEntity> generateFeedback({
     required String sessionId,
